0448f7ecdbf987990e1ef2b25fc9d0f529efa06a
unknown <jyang@ENG-CLINIC-2.HMC.Edu>
2014-11-13 14:19:53 -0800

    working repo. Need to work on multiple cameras

--- OverHead_Cameras/OpenCV_guitest/OpenCV_guitest/test.cpp
+++ OverHead_Cameras/OpenCV_guitest/OpenCV_guitest/test.cpp
@@ -12,64 +12,29 @@ http://www.engadget.com/products/sony/playstation/eye/specs/
 using namespace std;
 using namespace cv;
 
-int main(){
-	cout << "here it starts"<<endl;
-//	cout <<countCameras();
-	//create matrix to store image
-	Mat image0;
-	Mat image1;
-	
-	// initialize capture
-	VideoCapture cap0(0);
-	VideoCapture cap1(1);
-	cap0.open(0);
-	cout<<cap0.isOpened()<<endl;
-	//sleep
-	this_thread::sleep_for (chrono::milliseconds(1));
-	cap1.open(1);
-	cout<<cap1.isOpened()<<endl;
-	//create window to show image
-	namedWindow("window1", 0);
-	namedWindow("window2", 1);
-	
-	int lowH = 0;
-	int highH = 179;
-
-	int lowS = 0; 
-	int highS = 255;
-
-	int lowV = 0;
-	int highV = 255;
-
-	//Create trackbars in "Control" window
-	cvCreateTrackbar("LowH", "Control", &lowH, 179); //Hue (0 - 179)
-	cvCreateTrackbar("HighH", "Control", &highH, 179);
-
-	cvCreateTrackbar("LowS", "Control", &lowS, 255); //Saturation (0 - 255)
-	cvCreateTrackbar("HighS", "Control", &highS, 255);
-
-	cvCreateTrackbar("LowV", "Control", &lowV, 255); //Value (0 - 255)
-	cvCreateTrackbar("HighV", "Control", &highV, 255);
-
-	while(1){
-		cap0>>image0;
-		//this_thread::sleep_for (chrono::milliseconds(1));
-		cap1>>image1;
-		// print to screen
-		//findBox(image0, lowH, highH, lowS, highS, lowV, highV);
-		imshow("window1",image0);
-		//delay 33ms
-		waitKey(33);
-	}
-}
-
-
-int findBox(Mat frame, int &lowH, int &highH, int &lowS, int &highS, int &lowV, int &highV)
+Scalar findBox(Mat frame, int &lowH, int &highH, int &lowS, int &highS, int &lowV, int &highV)
 {
+	//Change and filter
 	Mat hsvFrame, threshFrame;
 	cvtColor(frame, hsvFrame, CV_RGB2HSV);
-	//inRange(hsvFrame,)
-	return 0;
+	inRange(hsvFrame,Scalar(lowH,lowS,lowV), Scalar(highH,highS,highV), threshFrame);
+	//Smoothing Functions
+	erode(threshFrame, threshFrame, getStructuringElement(MORPH_ELLIPSE,Size(5,5)));// remove objects
+	dilate(threshFrame,threshFrame, getStructuringElement(MORPH_ELLIPSE,Size(5,5)));
+		
+	dilate(threshFrame,threshFrame, getStructuringElement(MORPH_ELLIPSE,Size(5,5)));// fill gaps
+	erode(threshFrame, threshFrame, getStructuringElement(MORPH_ELLIPSE,Size(5,5)));
+	//Find Rectangel
+	//vector<vector<Point>> contoursFound;
+	//findContours(threshFrame, contoursFound, CV_RETR_EXTERNAL,CV_CHAIN_APPROX_SIMPLE);
+	//Scalar color( rand()&255, rand()&255, rand()&255 );
+	//drawContours(threshFrame,contoursFound,-2, color, CV_FILLED,8);
+	//Display Image
+
+	imshow("Thresholded Image", threshFrame);
+	imshow("Original", frame);
+
+	return Scalar(0,0);
 }
 
 
@@ -85,3 +50,51 @@ int countCameras(){
 	   }
    }
 }
+
+
+
+int main(){
+	cout << "here it starts"<<endl;
+//	cout <<countCameras();
+	//create matrix to store image
+	Mat image0;
+	Mat frame, threshFrame;
+	
+	// initialize capture
+	VideoCapture cap0(0);
+
+	cap0.open(0);
+	cout<<cap0.isOpened()<<endl;
+
+	namedWindow("Control", CV_WINDOW_AUTOSIZE); //create a window called "Control"
+	
+	//Inital values for lower,higher bounds for HSV.
+	int lowH = 68;
+	int highH = 131;
+
+	int lowS = 89; 
+	int highS = 255;
+
+	int lowV = 124;
+	int highV = 255;
+
+	//Create trackbars in "Control" window
+	cvCreateTrackbar("LowH", "Control", &lowH, 179); //Hue (0 - 179)
+	cvCreateTrackbar("HighH", "Control", &highH, 179);
+
+	cvCreateTrackbar("LowS", "Control", &lowS, 255); //Saturation (0 - 255)
+	cvCreateTrackbar("HighS", "Control", &highS, 255);
+
+	cvCreateTrackbar("LowV", "Control", &lowV, 255); //Value (0 - 255)
+	cvCreateTrackbar("HighV", "Control", &highV, 255);
+
+
+	while(1){
+		cap0>>image0;
+		Scalar box1=findBox(image0, lowH, highH, lowS, highS, lowV, highV);
+		//delay 33ms
+		waitKey(33);
+	}
+}
+
+
---
+++ OverHead_Cameras/OpenCV_guitest/OpenCV_guitest/test1.cpp
@@ -0,0 +1,58 @@
+#include <opencv\cv.h>
+#include <opencv\highgui.h>
+#include <iostream>
+#include <thread> 
+
+using namespace std;
+using namespace cv;
+
+
+
+
+int main(){
+	cout << "here it starts"<<endl;
+//	cout <<countCameras();
+	//create matrix to store image
+	Mat image1;
+	Mat image2;
+	// initialize capture
+	VideoCapture cap1;
+	VideoCapture cap2;
+	cap1.open(0);
+
+//	sleep();
+	cap2.open(1);
+
+	//create window to show image
+	namedWindow("window1", 1);
+
+	//namedWindow("window2", 1);
+
+	while(1){
+		cap1>>image1;
+		//cap2>>image2;
+		// print to screen
+		imshow("window1",image1);
+		//imshow("window2",image2);
+
+		//delay 33ms
+		waitKey(33);
+	}
+}
+
+
+
+
+
+int countCameras(){
+   VideoCapture temp_camera;
+   int maxTested = 10;
+   for (int i = 0; i < maxTested; i++){
+	   cv::VideoCapture temp_camera(i);
+	   bool res = (!temp_camera.isOpened());
+	   temp_camera.release();
+	   if (res){
+			return i;
+	   }
+   }
+}
