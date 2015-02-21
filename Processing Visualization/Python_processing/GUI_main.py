80e199355a1b5b32c13bb72a7a6b66ed1a83d4f7
unknown <jyang@ENG-CLINIC-2.HMC.Edu>
2015-02-20 16:13:52 -0800

    added buttons

--- Processing Visualization/Python_processing/GUI_main.py
+++ Processing Visualization/Python_processing/GUI_main.py
@@ -25,12 +25,51 @@ def runCamera():
 
 def buildGUI():
 	app = wx.App(False)
-	frame = wx.Frame(None, wx.ID_ANY, "HelloWorld")
-	frame.Show(True)
-	#app.MainLoop()
+	#frame = wx.Frame(None, wx.ID_ANY, "HelloWorld")
+	#frame.Show(True)
+	frame = MainWindow(None, "editor")
+	#frame = MyFrame(None, 'Small editor')
+	app.MainLoop()
+
+class MyFrame(wx.Frame):
+    """ We simply derive a new class of Frame. """
+    def __init__(self, parent, title):
+        wx.Frame.__init__(self, parent, title=title, size=(200,100))
+        self.control = wx.TextCtrl(self, style=wx.TE_MULTILINE)
+        self.Show(True)
+
+class MainWindow(wx.Frame):
+    def __init__(self, parent, title):
+        wx.Frame.__init__(self,parent, title=title, size=(200,100))
+        # initialize menu bar
+        self.control = wx.TextCtrl(self, style=wx.TE_MULTILINE)
+        self.CreateStatusBar() # A Statusbar in the bottom of the window
+        # Setting up the menu.
+        filemenu= wx.Menu()
+
+        # wx.ID_ABOUT and wx.ID_EXIT are standard IDs provided by wxWidgets.
+        filemenu.Append(wx.ID_ABOUT, "&About"," Information about this program")
+        filemenu.AppendSeparator()
+        filemenu.Append(wx.ID_EXIT,"E&xit"," Terminate the program")
+
+        # Creating the menubar.
+        menuBar = wx.MenuBar()
+        menuBar.Append(filemenu,"&File") # Adding the "filemenu" to the MenuBar
+        self.SetMenuBar(menuBar)  # Adding the MenuBar to the Frame content.
+  
+        #Add a butoon
+        menuItem = filemenu.Append(wx.ID_ABOUT, "&About"," Information about this program")
+        self.Bind(wx.EVT_MENU, self.OnAbout, menuItem)
+
+
+
+
+        self.Show(True)
+
+	def OnButtonClick(self, event):
+    	self.running == False
+
 
 if __name__ == '__main__':
-
-
-	runCamera()
-	#buidGui()
+	#runCamera()
+	buildGUI()
