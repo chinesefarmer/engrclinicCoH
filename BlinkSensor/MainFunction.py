32a88e02485e6330d559621f5161f17795f3ed98
Nicole Kowtko <akowtko@hmc.edu>
2015-04-12 10:47:00 -0700

    Spring_BlinkSensory.py function can now be imported into Ben's function to run properly, instead of copy and pasted in. If Nicole wants to run it, use MainFunction.py

---
+++ BlinkSensor/MainFunction.py
@@ -0,0 +1,41 @@
+test = False
+from Spring_BlinkSensor import *
+
+if __name__=='__main__':
+    if(test):
+        filename = ('2015-04-10Jeanette2.csv')
+        BlinkSensor = BlinkSensor()
+        BlinkSensor.filename = filename
+        BlinkSensor.saveBlinks([],1)
+        data = BlinkSensor.csv_reader(2)
+        Hour = data[2]
+        Minute = data[3]
+        #Time here means seconds and microseconds
+        Time = data[0]
+        RawIR = data[1]
+        for i in range(len(data[0])):
+            BlinkSensor.minTestMode = (Hour[i]*60 + Minute[i] + Time[i]/60)
+            BlinkSensor.timeTestMode = Time[i]
+            BlinkSensor.Algorithm(RawIR[i],True)
+        BlinkSensor.saveFile()
+        BlinkSensor.csv_reader(1)
+    else:
+        filename = raw_input('Enter a file name:  ')
+        timeDate = datetime.now().date()
+        filename = (str(timeDate)+filename+'.csv')
+
+        usb = Serial('/dev/cu.usbmodem621',57600)
+
+        BlinkSensor = BlinkSensor()
+        BlinkSensor.filename = filename
+        initSerialConnection(usb,BlinkSensor)
+
+
+        while True:
+            try:
+                getSerial(usb, BlinkSensor)
+            except KeyboardInterrupt:
+                BlinkSensor.saveFile()
+                BlinkSensor.csv_reader(1)
+                usb.close()
+                break
